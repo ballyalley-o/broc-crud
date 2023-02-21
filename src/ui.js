39b28d1a2fc2fb98507e759e57b5856fdbc4cd95
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-02-22 01:30:25 +1300

    json-server,  link ui, card

---
+++ src/ui.js
@@ -0,0 +1,30 @@
+class UI {
+    constructor() {
+        this.post = document.querySelector("#posts");
+        this.titleInput = document.querySelector("#title");
+        this.descArea = document.querySelector("#desc");
+        this.idInput = document.querySelector("#id");
+        this.postSubBtn = document.querySelector(".post-submit");
+        this.formState = 'add'
+    }
+
+    showPosts(posts) {
+        let output = '';
+
+        posts.forEach((post) => {
+            output += `
+            <div class="card mb-3">
+                <div class="card-body">
+                <h4 class="card-title">${post.title}</h3>
+                    <p class="card-text">${post.content}</p>
+                    <a href="# class="edit card-link"></a>
+                </div>
+            </div>`
+        })
+        this.post.innerHTML = output
+    }
+}
+
+
+
+export const ui = new UI();
